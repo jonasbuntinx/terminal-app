607c8e3adfa2cbc5c599bea5c80ba42e9b565124
Jonas Buntinx <buntinxjonas@gmail.com>
2021-04-12 10:37:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Terminal app
+
+A terminal app
+
+## Build Setup
+
+```npm
+# install dependencies
+$ npm i
+
+# run terminal app
+$ node index.js -i mock.xml -r Lorem Dorem -c 10 -o output.xml
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+const yargs = require( "yargs" );
+const fs = require('fs');
+const rssparser = require('rss-parser');
+const xml2js = require('xml2js');
+
+const options = yargs
+  .option( "i", { alias: "input", describe: "Your input", type: "string", demandOption: true } )
+  .option( "c", { alias: "cut", describe: "Trim each title and body if the length exceeds x characters", type: "number" } )
+  .option( "r", { alias: "replace", describe: "Replace the specified words", type: "array" } )
+  .option( "o", { alias: "output", describe: "Your output", type: "string" } )
+  .argv;
+
+const convert = (feed) => {
+
+  if(options.cut) {
+    feed.items = feed.items.map((item) => {
+      if(item.title){
+        item.title = item.title.substring(0, options.cut);
+      }
+      if(item.body){
+        item.body = item.body.substring(0, options.cut);
+      }
+      return item;
+    });
+  }
+
+  if(options.replace && options.replace.length >= 2) {
+    feed.items = feed.items.map((item) => {
+      if(item.title){
+        item.title = item.title.replace(options.replace[0], options.replace[1]);
+      }
+      if(item.body){
+        item.body = item.body.replace(options.replace[0], options.replace[1]);
+      }
+      return item;
+    });
+  }
+
+  if(options.output) {
+    const builder = new xml2js.Builder();
+    const xml = builder.buildObject(feed);
+    fs.writeFileSync(options.output, xml);
+  } else {
+    console.log(feed);
+  }
+}
+
+const isURL = (str) => {
+  var pattern = new RegExp('^(https?:\\/\\/)')
+  return !!pattern.test(str);
+}
+
+const parser = new rssparser();
+
+if(isURL(options.input)) { 
+  parser.parseURL(options.input).then(convert);
+}
+else {
+  const xml = fs.readFileSync(options.input, 'utf8');
+  parser.parseString(xml).then(convert);
+}
